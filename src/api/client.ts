c236001c174e99d49486a2aa027c1b79c391acd3
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-09-26 16:41:34 -0300

    new: configure md5 generateHash

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
-import { API_BASE } from "../env";
+import { API_BASE, MARVEL_PUBLIC_KEY, MARVEL_PRIVATE_KEY } from "../env";
+import md5 from "md5";
+
+function generateHash(timestamp: number): string {
+  const hash = md5(`${timestamp}${MARVEL_PRIVATE_KEY}${MARVEL_PUBLIC_KEY}`);
+  return hash;
+}
 
 export const api = axios.create({
   baseURL: API_BASE,
 });
+
--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,13 @@
 export const API_BASE = import.meta.env.VITE_API_BASE;
 export const MARVEL_PUBLIC_KEY = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
+export const MARVEL_PRIVATE_KEY = import.meta.env.MARVEL_PRIVATE_KEY;
 
+if (!API_BASE) {
+  console.warn("VITE_API_BASE não definida!!!.");
+}
 if (!MARVEL_PUBLIC_KEY) {
   console.warn("VITE_MARVEL_PUBLIC_KEY não definida!!!.");
+}
+if (!MARVEL_PRIVATE_KEY) {
+  console.warn("VITE_MARVEL_PRIVATE_KEY não definida!!!.");
 }
