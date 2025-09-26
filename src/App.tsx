58b734ef391f6af5f3ce42adb0879a73f64aca4a
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-09-26 10:54:13 -0300

    estilo main App module

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./global.css";
 import { Header } from "./components/header";
 import { SearchPersonagens } from "./components/searchPersonagens";
+import { ListPersonagens } from "./components/listPersonagens";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
       <Header />
-      <SearchPersonagens />
+      <main className={styles.main}>
+        <SearchPersonagens />
+        <ListPersonagens />
+      </main>
     </>
   );
 }
